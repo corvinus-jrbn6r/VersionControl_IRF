4d1be14ea50eea610474980d67bc04fb41eb61d3
szrek <szrek@LAPTOP-0HTU5LEN>
2021-10-08 11:59:24 +0200

    format

--- UserMaintenance/ExcelExport/ExcelExport/Form1.cs
+++ UserMaintenance/ExcelExport/ExcelExport/Form1.cs
@@ -21,6 +21,7 @@ namespace ExcelExport
         Excel.Application xlApp; // A Microsoft Excel alkalmazás
         Excel.Workbook xlWB; // A létrehozott munkafüzet
         Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+        string[] headers;
 
 
         public Form1()
@@ -29,7 +30,7 @@ namespace ExcelExport
             LoadData();
             CreateExcel();
 
-           
+
         }
 
         public void LoadData()
@@ -41,7 +42,7 @@ namespace ExcelExport
         {
             try
             {
-                
+
                 // Excel elindítása és az applikáció objektum betöltése
                 xlApp = new Excel.Application();
 
@@ -53,6 +54,7 @@ namespace ExcelExport
 
                 // Tábla létrehozása
                 CreateTable(); // Ennek megírása a következő feladatrészben következik
+                FormatTable();
 
                 // Control átadása a felhasználónak - miután megnyitottuk és írtunk a munkalapra
                 xlApp.Visible = true; //megjelenítjük az eddig háttérben futó alkalmazást
@@ -75,12 +77,91 @@ namespace ExcelExport
                 MessageBox jöhet ide
             }*/
 
-            
+
         }
         private void CreateTable()
         {
+            // ha nem sorolom fel, akkor meg kell adni a számot a kapcsos zárójelben
+           headers = new string[] {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
 
+            for (int i = 0; i < headers.Length; i++)
+                xlSheet.Cells[1, i + 1] = headers[i];
+
+            // xlSheet.Cells[1, 1] = headers[0];  ezzel lehetne beírni egy cellába, a fentebbivel az össszesbe
+
+            object[,] values = new object[lakasok.Count, headers.Length]; //így mégcsak VS oldalon van
+
+            int counter = 0;
+            int floorcolumn = 6;
+            foreach (var lakas in lakasok)
+            {
+                values[counter, 0] = lakas.Code; // 0-tól indexálom az oszlopokat
+                values[counter, 1] = lakas.Vendor;
+                values[counter, 2] = lakas.Side;
+                values[counter, 3] = lakas.District;
+
+                if (lakas.Elevator)
+                    values[counter, 4] = "Van";
+                else
+                    values[counter, 4] = "Nincs";
+
+                values[counter, 5] = lakas.NumberOfRooms;
+                values[counter, 6] = lakas.FloorArea;
+                values[counter, 7] = lakas.Price;
+                values[counter, 8] = string.Format("={0}/{1}*{2}", "H" + (counter + 2).ToString(),
+                    GetCell((counter + 2), floorcolumn + 1), 1000000);
+                //"=H2/G2*1000000";
+
+                counter++;
+            }
+            //ezzel egy range-et választottam ki a sheeten
+            var range = xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(1 + values.GetLength(0), values.GetLength(1)));
+
+            //value 2 kell az excelhez, amikor object valami
+            range.Value2 = values;
         }
 
+        private string GetCell(int x, int y) //ezt nem kell tudnom, de fel kell tudnom használni (tudnom kell, mi ez)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void FormatTable(){
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            Excel.Range completeTableRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, headers.Length));
+            completeTableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
     }
 }
