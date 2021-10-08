29933d1afb86671c3a32ed78613cf45a39a0a495
szrek <szrek@LAPTOP-0HTU5LEN>
2021-10-08 10:48:38 +0200

    create excel APP and WB

--- UserMaintenance/ExcelExport/ExcelExport/Form1.cs
+++ UserMaintenance/ExcelExport/ExcelExport/Form1.cs
@@ -17,11 +17,17 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
-            dataGridView1.DataSource = lakasok;
+            CreateExcel();
 
            
         }
@@ -30,5 +36,51 @@ namespace ExcelExport
         {
             lakasok = context.Flats.ToList();
         }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value); // mert egy üres doksit hozunk létre
+
+                // Új munkalap, az a munkalap, ami aktuálisan meg van nyitva (amikor új excelt nyitok, lesz egy új munkalapunk)
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak - miután megnyitottuk és írtunk a munkalapra
+                xlApp.Visible = true; //megjelenítjük az eddig háttérben futó alkalmazást
+                xlApp.UserControl = true; //átadjuk a vezérlőt
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error"); //\nLine a sortörés
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing); //bezárom a munkafüzetet
+                //első: mentsük el a hibás állomány, hova mentsem, ez is hogy hova mentsem)
+                xlApp.Quit(); //ezzel lehet teljesen lezárni a dolgot, bezárom az appot
+                xlWB = null; //törlöm a munkafüzetet
+                xlApp = null; //törlöm az appot
+            }
+            /*finally //akkor is lefut, ha a catch-el elkaptunk egy hibát
+            {
+                MessageBox jöhet ide
+            }*/
+
+            
+        }
+        private void CreateTable()
+        {
+
+        }
+
     }
 }
